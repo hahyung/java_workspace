ad871bee216f60665f06922e525d6b7c120951c8
donechoi <xana_@naver.com>
2022-06-29 21:44:19 +0900

    java_workspace

---
+++ test01/src/test01/Test.java
@@ -0,0 +1,16 @@
+package test01;
+
+interface TestIn{
+	public void test();
+}
+class Test implements TestIn{
+	public Test() {
+		
+	}
+
+	@Override
+	public void test() {
+		// TODO Auto-generated method stub
+		
+	}
+}
